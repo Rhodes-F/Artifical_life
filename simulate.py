669722f85b5ad5f52f8a49b6cb25d3557f50bbdc
Rhodes-F <102707768+Rhodes-F@users.noreply.github.com>
2023-01-09 10:52:43 -0600

    adding a box

--- simulate.py
+++ simulate.py
@@ -2,9 +2,10 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(1./200.)
+    time.sleep(1./60.)
     print(i)
 
 p.disconnect()
